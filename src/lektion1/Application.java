34c804636994ab8560602cb5b872b4962f68b35f
Robin Jönsson <robin.jonsson@gritacademy.se>
2024-10-21 19:28:52 +0200

    Lektion1

--- src/lektion1/Application.java
+++ src/lektion1/Application.java
@@ -1,61 +1,45 @@
 package lektion1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
-        //CirkelOmkrets();
-        //CirkelArea();
-        //VolymKlot();
-        //VolymCylinder();
-    }
 
-    private static void CirkelOmkrets() {
-        final double PI = 3.14; //Math.PI
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ange radien på cirkeln: ");
-        double radie = scanner.nextDouble();
-        double omkrets = 2 * PI * radie;
-        System.out.printf("Omkretsen av cirkeln med radie %.2f är %.2f%n", radie, omkrets);
-        scanner.close();
-    }
+        Random random = new Random();
 
-    private static void CirkelArea() {
+        //Vi skapar ett random-tal. I detta fall något som
+        // skall representera en radie, med decimal.
+        // Vi anger 50.0 för att vi vill sätta en maxgräns på max 50
+        // Vi lägger till +1 för att börja räkna från 1 istället för från 0
+        // Vi använder "f" efter 1.0 för att berätta att det är ett flyttal/decimal
+        double radie = random.nextDouble(50.0) + 1.0f;
+
+        // Deklarerar och initierar PI, variable
         final double PI = 3.14;
+
+        // Använder oss av javas Scanner för att ta emot user-input
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ange radien på cirkeln: ");
-        double radie = scanner.nextDouble();
+
+        // Skapar variabler för att spara undan uträkningar
         double omkrets = 2 * PI * radie;
         double area = PI * radie * radie;
-        System.out.println("Omkretsen av cirkeln är: " + omkrets);
-        System.out.println("Arean av cirkeln är: " + area);
-    }
 
-    private static void VolymKlot() {
-        final double PI = 3.14;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ange radien på cirkeln: ");
-        double radie = scanner.nextDouble();
-        double omkrets = 2 * PI * radie;
-        double area = PI * radie * radie;
-        System.out.println("Omkretsen av cirkeln är: " + omkrets);
-        System.out.println("Arean av cirkeln är: " + area);
-    }
-
-    private static void VolymCylinder() {
-        final double PI = 3.14;
-        Scanner scanner = new Scanner(System.in);
-        double radie = scanner.nextDouble();
-        double omkrets = 2 * PI * radie;
-        double area = PI * radie * radie;
+        // Math.pow kan användas när vi skall räkna ut "upphöjt till"
+        // I nedan fall har vi alltså radien upphöjt till 3
         double volymKlot = (4.0 / 3.0) * PI * Math.pow(radie, 3);
         System.out.println("Omkretsen av cirkeln är: " + omkrets);
         System.out.println("Arean av cirkeln är: " + area);
         System.out.println("Volymen av klotet är: " + volymKlot);
         System.out.print("Vill du beräkna volymen av en cylinder? (ja/nej): ");
-        String svar = scanner.nextLine();
 
+        //Vi fångar upp användaren inmatning och sparar undan i variabeln svar
+        String svar = scanner.next();
+
+        // Kontrollerar vad användaren vill göra
+        // Om ja - körs första kodblock (innanför if)
+        // annars körs kodblock innanför else
         if (svar.equalsIgnoreCase("ja")) {
             System.out.print("Ange höjden på cylindern: ");
             double hojd = scanner.nextDouble();
@@ -64,6 +48,17 @@ public class Application {
         } else {
             System.out.println("Inga fler beräkningar görs.");
         }
+        /**
+         * I Java används scanner.close() för att stänga en Scanner-instans när du är klar med att använda den. Det har flera syften:
+         *
+         * Resurshantering: När en Scanner skapas, t.ex. för att läsa från en fil eller systemets inmatning (som System.in), allokeras resurser som minne och filhandtag. Genom att stänga scannern frigörs dessa resurser.
+         *
+         * Undvika minnesläckor: Om du inte stänger scannern kan det leda till att programmet använder mer minne än nödvändigt, vilket kan påverka prestandan.
+         *
+         * Stängning av strömmar: Om Scanner är kopplad till en fil eller annan ström, stänger scanner.close() också den kopplade strömmen. Det är viktigt för att säkerställa att alla data skrivs och att filen inte är låst.
+         *
+         * Det är en god praxis att alltid stänga resurser när de inte längre behövs för att undvika potentiella problem.
+         */
         scanner.close();
     }
 }
---
+++ src/lektion1/Lektion1.md
@@ -0,0 +1,38 @@
+# Lektion 1
+
+## Sammanfattning
+Idag har vi gått igenom datorns delar, hur de fungerar ihop och varför de behövs.
+Vi har pratat lite om varför det är viktigt att ha koll på och vad vi som programmerare
+behöver tänka på. Vi har även gjort mycket praktiska övningar gällande `System.out`, `Scanner`, 
+`random`. Detta kommer behövas för inlämningsuppift 1.
+
+### Att lägga på minnet
+Vi lever i en tid då minne varit en billig vara och vi har varit bortskämda med att kunna
+nyttja oändligt av detta. Emellertid så har detta indirekt börjat bli dyrare då många ger sig 
+på resan ut i molnet (Cloud). AWS, Azure och liknande plattsformat tar betalt för klockcykler och
+minnesanvändning. Därmed så bör vi ganska tidigt bli medvetna, som utvecklare, kring att skriva
+bra och grön kod. Detta är inget vi mer djupgående gått in i, men kommer nudda vid, kommande lektion.
+
+### Praktisk öving
+Dagen har mestadels bestått av praktisk övning. Detta givetvis i kombination med teori. Upplägget
+är liknande framöver som idag. För mig är det viktigt att ni aktivt är med och praktiskt kommer in i
+koden, snarare än att bara lyssna på teori och se exempel.
+
+### Kommande lektioner
+Kommande kommer vi ha lektioner om variabler och typer. Här kommer det bli fortsättning på minne och minneshantering.
+Vi kommer också titta vidare på metoder och loopar denna vecka.
+
+### Inlämningsuppgiften
+Inlämningsuppgiften till nästa måndag är idag presenterad. ***Känn ingen stress***. Vi har precis haft den första
+lektionen. Det räknas ***inte*** med att ni skall lösa uppgiften första dagen. DOCK, är det bra att påbörja den, så
+ni kan ställa frågor och få handledning i rätt tid. Vi kommer gå igenom resterande av det som saknas för att lösa 
+uppgiften, innan veckan är slut.
+
+### Till slut
+Tack för idag! Himla trevligt att komma till en klass med så stort engagemang och öppenhet för diskussion.
+Jag uppskattar feedback på dagens lektion och det går bra att maila mig feedback. Gärna om du anser att upplägget
+mått bra av förändring. Alla lär sig på olika sätt och jag är ute efter att nå ut till er alla.
+
+Bra jobbat!
+
+robin.jonsson@gritacademy.se
